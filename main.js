3cd7fc386feda3657833efe2d5eaee2160f362b6
Shannon Coache <scoach@saic.edu>
2019-10-06 23:11:05 -0500

    Add files

---
+++ main.js
@@ -0,0 +1,34 @@
+
+// 1. Use a for-loop to print out your name 6 times with a number on the end
+
+for (let i = 0; i < 6; i++) {
+    console.log(`Shannon${i}`)
+}
+
+
+
+// 2. Use a while-loop to print out a string of incrementing numbers
+
+let j = 0;
+
+while(j< 10) {
+    console.log(j);
+    j++;
+}
+
+
+
+
+
+// 3. Use a for-loop and some conditional statements (if/else) to print out this pattern: +-+
+
+
+for (let i = 0; i < 10; i++) {
+    console.log(`I had ${i} apples today`)
+}
+
+
+
+
+
+// 4. (bonus). Use a nested for-loop to print out rows of increasing stars:
